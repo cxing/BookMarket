b641b24e29a15ec698145bff521d5a46f3ec7f3f
Changxu Xing <xcx_xhgj@163.com>
2016-10-12 20:25:51 +0800

    add booklist tables and config mapping xml files

--- src/main/java/com/web/entity/Book.java
+++ src/main/java/com/web/entity/Book.java
@@ -5,49 +5,133 @@ package com.web.entity;
  */
 public class Book {
 
-	private long book_id;// 图书ID
+	private String id;			// 图书ID
 
-	private String name;// 图书名称
+	private int cid;  			//图书分类ID
 
-	private int number;// 馆藏数量
+	private String bname;		// 图书名称
+
+	private String author;		//作者
+
+	private float price;		//价格
+
+	private String press;		//出版社
+
+	private String publishTime;	//出版时间
+
+	private int edition;		//版本
+
+	private int pageNum;		//页数
+
+	private int wordNum;		//字数
+
+	private String printTime;	//印刷时间
+
+	private String image;		//图片
 
 	public Book() {
 	}
 
-	public Book(long book_id, String name, int number) {
-		this.book_id = book_id;
-		this.name = name;
-		this.number = number;
+	public String getId() {
+		return id;
 	}
 
-	public long getBookId() {
-		return book_id;
+	public void setId(String id) {
+		this.id = id;
 	}
 
-	public void setBookId(long book_id) {
-		this.book_id = book_id;
+	public int getCid() {
+		return cid;
 	}
 
-	public String getName() {
-		return name;
+	public void setCid(int cid) {
+		this.cid = cid;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getBname() {
+		return bname;
 	}
 
-	public int getNumber() {
-		return number;
+	public void setBname(String bname) {
+		this.bname = bname;
 	}
 
-	public void setNumber(int number) {
-		this.number = number;
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getPress() {
+		return press;
+	}
+
+	public void setPress(String press) {
+		this.press = press;
+	}
+
+	public String getPublishTime() {
+		return publishTime;
+	}
+
+	public void setPublishTime(String publishTime) {
+		this.publishTime = publishTime;
+	}
+
+	public int getEdition() {
+		return edition;
+	}
+
+	public void setEdition(int edition) {
+		this.edition = edition;
+	}
+
+	public int getPageNum() {
+		return pageNum;
+	}
+
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+
+	public int getWordNum() {
+		return wordNum;
+	}
+
+	public void setWordNum(int wordNum) {
+		this.wordNum = wordNum;
+	}
+
+	public String getPrintTime() {
+		return printTime;
+	}
+
+	public void setPrintTime(String printTime) {
+		this.printTime = printTime;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
 	}
 
 	@Override
 	public String toString() {
-		return "Book [book_id=" + book_id + ", name=" + name + ", number=" + number + "]";
+		return "Book [id=" + id + ", cid=" + cid + ", bname=" + bname + ", author=" + author + ", price=" + price
+				+ ", press=" + press + ", publishTime=" + publishTime + ", edition=" + edition + ", pageNum=" + pageNum
+				+ ", wordNum=" + wordNum + ", printTime=" + printTime + ", image=" + image + "]";
 	}
-
-
 }
